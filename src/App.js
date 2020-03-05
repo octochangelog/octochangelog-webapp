6df3aca5f854534c42764c6e3f30aeff8220122f
Mario Beltrán Alarcón <belco90@gmail.com>
2020-03-05 22:09:12 +0100

    hello world from markdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import './App.css';
 
-const App = () => 'github-compare-releases';
+const Converter = require('md-to-bemjson');
+const md2Bemjson = new Converter();
+
+const App = () => (
+  <pre>{JSON.stringify(md2Bemjson.convertSync('# Hello world'), null, 2)}</pre>
+);
 
 export default App;
