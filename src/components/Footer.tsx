c6699824086648327e969aa2450fc854f408c239
Mario Beltrán Alarcón <belco90@gmail.com>
2020-04-10 19:37:38 +0200

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Box, Link } from '@chakra-ui/core';
+import { Box, Flex, Link } from '@chakra-ui/core/dist';
 import Container from './Container';
 
 const Footer = () => {
   return (
     <Box as="footer" bg="gray.50" flexShrink={0}>
       <Container py={5}>
-        <span>
-          Built with{' '}
-          <span role="img" aria-label="Love">
-            🧡
-          </span>{' '}
-          by{' '}
-          <Link href="https://mario.dev/" color="orange.500" isExternal>
-            Mario
-          </Link>
-        </span>
+        <Flex justify="space-between">
+          <Box>
+            Created with{' '}
+            <span role="img" aria-label="Love">
+              🧡
+            </span>{' '}
+            by{' '}
+            <Link href="https://mario.dev/" color="orange.500" isExternal>
+              Mario
+            </Link>
+          </Box>
+          <Box>
+            <Link
+              className="github-button"
+              href="https://github.com/Belco90/github-compare-releases"
+              data-icon="octicon-star"
+              data-size="large"
+              data-show-count="true"
+              aria-label="Star Belco90/github-compare-releases on GitHub"
+            >
+              Star
+            </Link>
+          </Box>
+        </Flex>
       </Container>
     </Box>
   );
