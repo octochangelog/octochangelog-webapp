1be1075b19b166982ac3abfc9ee22d20245c727e
Mario Beltrán Alarcón <belco90@gmail.com>
2020-04-27 20:54:00 +0200

    Include env vars in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  env: {},
+  env: {
+    GITHUB_APP_CLIENT_ID: process.env.GITHUB_APP_CLIENT_ID,
+    GITHUB_APP_CLIENT_SECRET: process.env.GITHUB_APP_CLIENT_SECRET,
+  },
   webpack: (config) => {
     config.plugins = config.plugins || [];
     config.plugins.push(new Dotenv({ silent: true }));
