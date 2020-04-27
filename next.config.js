e61bb8fa2bc2a1c5e133bfcce5004df692aa3090
Mario Beltrán Alarcón <belco90@gmail.com>
2020-04-27 18:33:55 +0200

    Revert removing dotenv webpack

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-//const Dotenv = require('dotenv-webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   env: {},
   webpack: (config) => {
     config.plugins = config.plugins || [];
-    //config.plugins.push(new Dotenv({ silent: true }));
+    config.plugins.push(new Dotenv({ silent: true }));
 
     config.node = {
       fs: 'empty',
