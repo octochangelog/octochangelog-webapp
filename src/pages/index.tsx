287dbef7fc9f504d5ac007b72ae93a7183a85812
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-03 19:54:57 +0200

    Include curved banner on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
+import { Box } from '@chakra-ui/core';
 import React from 'react';
 
+import Container from '~/components/Container';
 import GitHubLoginLink from '~/components/GitHubLoginLink';
 import Layout from '~/components/Layout';
 
 const IndexPage = () => (
-  <Layout isHeaderFixed extraTitle="Home">
-    <GitHubLoginLink />
+  <Layout isHeaderFixed>
+    <Box
+      position="relative"
+      mt={-8}
+      py={16}
+      bg="primary.500"
+      height="50vh"
+      borderBottomLeftRadius="50% 20%"
+      borderBottomRightRadius="50% 20%"
+    >
+      <Container>
+        <GitHubLoginLink />
+      </Container>
+    </Box>
   </Layout>
 );
 
