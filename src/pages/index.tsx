95f3081411d2db0df6946b32349c7bd0dc896f00
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-07 18:31:22 +0200

    Switch Features columns order

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,44 +58,45 @@ const IndexPage = () => (
 
     <Box my={24}>
       <Container>
-        <Heading as="h2" fontSize="3xl" mb={8}>
-          Main Features
-        </Heading>
         <SimpleGrid
           columns={{ base: 1, md: 2 }}
           spacing={{ base: 4, md: 8 }}
           alignItems="start"
         >
-          <List spacing={{ base: 4, md: 8 }}>
-            <ListItem>
-              <ListIcon icon="check-circle" color="primary.500" />
-              Filter and group several releases by same type of changes for
-              comparing them at once with ease
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="check-circle" color="primary.500" />
-              Highlight syntax for code blocks
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="check-circle" color="primary.500" />
-              Sort by different level of changes for{' '}
-              <Link href="https://semver.org/">Semantic Versioning</Link>,
-              specially those using{' '}
-              <Link href="https://semantic-release.gitbook.io/semantic-release/">
-                semantic-release
-              </Link>
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="check-circle" color="primary.500" />
-              Spot original version where specific changes were introduced
-            </ListItem>
-          </List>
-
           <Image
             src="https://via.placeholder.com/840x526.png"
             alt="App comparator screenshot"
             rounded={10}
           />
+          <Box>
+            <Heading as="h2" fontSize="3xl" mb={8}>
+              Main Features
+            </Heading>
+            <List spacing={{ base: 4, md: 8 }}>
+              <ListItem>
+                <ListIcon icon="check-circle" color="primary.500" />
+                Filter and group several releases by same type of changes for
+                comparing them at once with ease
+              </ListItem>
+              <ListItem>
+                <ListIcon icon="check-circle" color="primary.500" />
+                Highlight syntax for code blocks
+              </ListItem>
+              <ListItem>
+                <ListIcon icon="check-circle" color="primary.500" />
+                Sort by different level of changes for{' '}
+                <Link href="https://semver.org/">Semantic Versioning</Link>,
+                specially those using{' '}
+                <Link href="https://semantic-release.gitbook.io/semantic-release/">
+                  semantic-release
+                </Link>
+              </ListItem>
+              <ListItem>
+                <ListIcon icon="check-circle" color="primary.500" />
+                Spot original version where specific changes were introduced
+              </ListItem>
+            </List>
+          </Box>
         </SimpleGrid>
       </Container>
     </Box>
