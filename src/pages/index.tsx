4819a13cbd1a653cbb034627f27ceb791dd2723a
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-10 21:42:53 +0200

    Update landing page description section layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Flex,
   Heading,
   Image,
   List,
@@ -17,89 +18,105 @@ import Link from '~/components/Link';
 import MainLogo from '~/components/MainLogo';
 import customTheme from '~/customTheme';
 import { APP_MOTTO } from '~/global';
+import useWindowWidth from '~/hooks/useWindowWidth';
 
-const IndexPage = () => (
-  <Layout isHeaderFixed>
-    <Box
-      position="relative"
-      mt={-8}
-      pt={{ base: 8, md: 16 }}
-      minHeight="100vh"
-      bgImage={`linear-gradient(180deg, ${customTheme.colors.primary[700]} 0%, ${customTheme.colors.white} 100%)`}
-    >
-      <Container>
-        <SimpleGrid
-          columns={{ base: 1, md: 2 }}
-          spacing={{ base: 8, md: 4 }}
-          alignItems="center"
-        >
-          <Stack
-            alignItems={{ base: 'center', md: 'start' }}
-            spacing={{ base: 4, md: 8 }}
+const DESKTOP_BREAKPOINT = 992;
+
+const IndexPage = () => {
+  const windowWidth = useWindowWidth();
+  const isDesktop = windowWidth >= DESKTOP_BREAKPOINT;
+
+  return (
+    <Layout isHeaderFixed>
+      <Box
+        mt={-8}
+        pt={{ base: 8, lg: 16 }}
+        minHeight="100vh"
+        bgImage={`linear-gradient(180deg, ${customTheme.colors.primary[700]} 0%, ${customTheme.colors.white} 100%)`}
+      >
+        <Container>
+          {
+            <Flex
+              opacity={!!windowWidth ? 1 : 0} // avoid shifting layout while getting window width
+              alignItems="center"
+              direction={isDesktop ? 'row' : 'column-reverse'}
+              transition="opacity 500ms ease-in-out"
+            >
+              <Stack
+                alignItems={{ base: 'center', lg: 'start' }}
+                spacing={{ base: 4, lg: 8 }}
+                flexGrow={1}
+                flexBasis={0}
+              >
+                <Heading
+                  as="h1"
+                  color={isDesktop ? 'gray.50' : 'gray.700'}
+                  fontSize="4xl"
+                  textAlign={{ base: 'center', lg: 'left' }}
+                >
+                  {APP_MOTTO}
+                </Heading>
+                <GitHubLoginLink />
+              </Stack>
+              <Box flexGrow={1} flexBasis={0} maxWidth={600} maxHeight="auto">
+                <MainLogo />
+              </Box>
+            </Flex>
+          }
+        </Container>
+      </Box>
+
+      <Box my={{ base: 12, lg: 24 }}>
+        <Container>
+          <SimpleGrid
+            columns={{ base: 1, lg: 2 }}
+            spacing={8}
+            alignItems="start"
           >
-            <Heading
-              as="h1"
-              color="gray.50"
-              fontSize="4xl"
-              textAlign={{ base: 'center', md: 'left' }}
-            >
-              {APP_MOTTO}
-            </Heading>
-            <GitHubLoginLink />
-          </Stack>
-          <Box>
-            <MainLogo />
-          </Box>
-        </SimpleGrid>
-      </Container>
-    </Box>
-
-    <Box my={{ base: 12, md: 24 }}>
-      <Container>
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8} alignItems="start">
-          <Image
-            src="https://via.placeholder.com/840x526.png"
-            alt="App comparator screenshot"
-            rounded={8}
-          />
-          <Box>
-            <Heading
-              as="h2"
-              fontSize="3xl"
-              mb={{ base: 4, md: 8 }}
-              color="primary.500"
-            >
-              Main Features
-            </Heading>
-            <List spacing={{ base: 4, md: 8 }}>
-              <ListItem>
-                <ListIcon icon="check-circle" color="secondary.500" />
-                Filter by range of versions
-              </ListItem>
-              <ListItem>
-                <ListIcon icon="check-circle" color="secondary.500" />
-                Group changes by normalized categories (e.g. put "bug fixes" and
-                "minor" under the same group)
-              </ListItem>
-              <ListItem>
-                <ListIcon icon="check-circle" color="secondary.500" />
-                Sort and prioritize by different level of changes for{' '}
-                <Link href="https://semver.org/">Semantic Versioning</Link>
-              </ListItem>
-              <ListItem>
-                <ListIcon icon="check-circle" color="secondary.500" />
-                Highlight code blocks syntax and GitHub references
-              </ListItem>
-              <ListItem>
-                <ListIcon icon="check-circle" color="secondary.500" />
-                Spot original version where specific changes were introduced
-              </ListItem>
-            </List>
-          </Box>
-        </SimpleGrid>
-      </Container>
-    </Box>
-  </Layout>
-);
+            <Image
+              src="https://via.placeholder.com/840x526.png"
+              alt="App comparator screenshot"
+              rounded={8}
+            />
+            <Box>
+              <Heading
+                as="h2"
+                fontSize="3xl"
+                mb={{ base: 4, lg: 8 }}
+                color="primary.500"
+              >
+                Main Features
+              </Heading>
+              <List spacing={{ base: 4, lg: 8 }}>
+                <ListItem>
+                  <ListIcon icon="check-circle" color="secondary.500" />
+                  Filter by range of versions
+                </ListItem>
+                <ListItem>
+                  <ListIcon icon="check-circle" color="secondary.500" />
+                  Group changes by normalized categories (e.g. put "bug fixes"
+                  and "minor" under the same group)
+                </ListItem>
+                <ListItem>
+                  <ListIcon icon="check-circle" color="secondary.500" />
+                  Sort and prioritize by different level of changes for{' '}
+                  <Link href="https://semver.org/">Semantic Versioning</Link>
+                </ListItem>
+                <ListItem>
+                  <ListIcon icon="check-circle" color="secondary.500" />
+                  Highlight code blocks syntax and GitHub references
+                </ListItem>
+                <ListItem>
+                  <ListIcon icon="check-circle" color="secondary.500" />
+                  Spot original version where specific changes were introduced
+                </ListItem>
+              </List>
+            </Box>
+          </SimpleGrid>
+        </Container>
+      </Box>
+    </Layout>
+  );
+};
 
 export default IndexPage;
