a639933b529756a48273899218d70d45a035ade6
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-10 17:54:33 +0200

    Update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,75 +16,78 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="/apple-icon-57x57.png"
+            href="/favicon/apple-icon-57x57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="60x60"
-            href="/apple-icon-60x60.png"
+            href="/favicon/apple-icon-60x60.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="/apple-icon-72x72.png"
+            href="/favicon/apple-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/apple-icon-76x76.png"
+            href="/favicon/apple-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="/apple-icon-114x114.png"
+            href="/favicon/apple-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="/apple-icon-120x120.png"
+            href="/favicon/apple-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="/apple-icon-144x144.png"
+            href="/favicon/apple-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="/apple-icon-152x152.png"
+            href="/favicon/apple-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-icon-180x180.png"
+            href="/favicon/apple-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/android-icon-192x192.png"
+            href="/favicon/android-icon-192x192.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
-            href="/favicon-96x96.png"
+            href="/favicon/favicon-96x96.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#ac35b0" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta
+            name="msapplication-TileImage"
+            content="/favicon/ms-icon-144x144.png"
+          />
           <meta name="theme-color" content="#ac35b0" />
           <meta name="description" content={APP_MOTTO} />
           <meta name="keywords" content="GitHub Release Repository Changelog" />
@@ -95,7 +98,7 @@ class MyDocument extends Document {
           <meta name="twitter:title" content="Octoclairvoyant" />
           <meta
             name="twitter:image"
-            content="https://octoclairvoyant.now.sh/logo.png"
+            content="https://octoclairvoyant.now.sh/mascot-icon.png"
           />
           <meta name="twitter:creator" content="@belcoDev" />
           <meta name="twitter:description" content={APP_MOTTO} />
