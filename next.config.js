6de6de3093edda8dc7e8400a0eea220af8446c3d
Mario Beltrán Alarcón <belco90@gmail.com>
2020-11-03 18:07:52 +0100

    Redirect comparator url in SSR rather than client side

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/comparator',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config) => {
     config.node = {
       fs: 'empty',
