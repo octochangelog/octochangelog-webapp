10c59f8921680e620813f4b1b56be8e9aca66210
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-06-28 14:03:03 +0200

    Initial setup for new color scheme

--- src/customTheme.ts
+++ src/customTheme.ts
@@ -13,6 +13,10 @@ type ColorHues = {
   '900': string
 }
 
+// ***** Legacy colorscheme *****
+
+// primaryColor will be replaced with a suitable hue out of the Fuchsia TailwindCSS color palette.
+
 const primaryColor: ColorHues = {
   50: '#ffe8ff',
   100: '#efc2f0',
@@ -25,6 +29,8 @@ const primaryColor: ColorHues = {
   800: '#3b0f3d',
   900: '#180319',
 }
+
+// blueColor will be replaced with a suitable hue out of the Sky TailwindCSS color palette.
 
 const blueColor: ColorHues = {
   50: '#def8ff',
@@ -39,11 +45,80 @@ const blueColor: ColorHues = {
   900: '#00131b',
 }
 
+// ***** New colorscheme *****
+
+// coolGray contains all the shades of gray we'll use in the app.
+// Example uses: body text, headings, changelog text, page background.
+// https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+
+const tailwind_coolGray: ColorHues = {
+  50: '#F9FAFB',
+  100: '#F3F4F6',
+  200: '#E5E7EB',
+  300: '#D1D5DB',
+  400: '#9CA3AF',
+  500: '#6B7280',
+  600: '#4B5563',
+  700: '#374151',
+  800: '#1F2937',
+  900: '#111827',
+}
+
+// Fuchsia is our primary accent.
+// Example uses: call to action button, primary action button.
+// https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+
+const tailwind_Fuchsia: ColorHues = {
+  50: '#FDF4FF',
+  100: '#FAE8FF',
+  200: '#F5D0FE',
+  300: '#F0ABFC',
+  400: '#E879F9',
+  500: '#D946EF',
+  600: '#C026D3',
+  700: '#A21CAF',
+  800: '#86198F',
+  900: '#701A75',
+}
+
+// Sky is our secondary accent.
+// Example uses: secondary button, version badge.
+// https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+
+const tailwind_Sky: ColorHues = {
+  50: '#F0F9FF',
+  100: '#E0F2FE',
+  200: '#BAE6FD',
+  300: '#7DD3FC',
+  400: '#38BDF8',
+  500: '#0EA5E9',
+  600: '#0284C7',
+  700: '#0369A1',
+  800: '#075985',
+  900: '#0C4A6E',
+}
+
+// These are our normal text colors.
+// Do NOT use when you have text that goes on a colored background.
+// Text on colored backgrounds should use a suitable hue from the background color scheme.
+
+const primaryText = '#111827'
+const secondaryText = '#4B5563'
+const tertiaryText = '#6B7280'
+
+// ***** Collect constants, put them in a customTheme *****
+
 const customTheme = extendTheme({
   colors: {
     blue: blueColor,
     primary: primaryColor,
     secondary: blueColor,
+    coolGray: tailwind_coolGray,
+    fuchsia: tailwind_Fuchsia,
+    sky: tailwind_Sky,
+    primaryText: primaryText,
+    secondaryText: secondaryText,
+    tertiaryText: tertiaryText,
   },
   fonts: {
     heading: '"Open Sans", sans-serif;',
