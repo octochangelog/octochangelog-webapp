40dfb62203d1c78763e4090618e8f683500643cb
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-07-02 11:20:46 +0200

    Use IBM Plex Mono font

--- src/customTheme.ts
+++ src/customTheme.ts
@@ -48,6 +48,7 @@ const customTheme = extendTheme({
   fonts: {
     heading: '"Inter", sans-serif;',
     body: '"Inter", sans-serif;',
+    mono: '"IBM Plex Mono", monospace;',
   },
   styles: {
     global: {
