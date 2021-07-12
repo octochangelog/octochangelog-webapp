ea0be4587df6b6c32c5b5293b97af1d63ee97506
Mario Beltrán Alarcón <belco90@gmail.com>
2021-07-12 18:33:49 +0200

    setup Sentry with wizard package

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+const { withSentryConfig } = require('@sentry/nextjs')
 
-module.exports = withBundleAnalyzer({
+const config = withBundleAnalyzer({
   async redirects() {
     return [
       {
@@ -14,3 +15,17 @@ module.exports = withBundleAnalyzer({
     ]
   },
 })
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: process.env.NODE_ENV !== 'production', // Suppresses all logs in envs other than production
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+module.exports = withSentryConfig(config, SentryWebpackPluginOptions)
