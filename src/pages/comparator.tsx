2d6ae6069b850dadea3700e73d4caf15ce483e59
Mario Beltrán Alarcón <belco90@gmail.com>
2021-07-16 19:36:55 +0200

    Switch current route URLs
    The changes are:
    - root page (comparator) has been moved to /comparator
    - /about has been moved to root page

---
+++ src/pages/comparator.tsx
@@ -0,0 +1,16 @@
+import { NextSeo } from 'next-seo'
+
+import Layout from '~/components/Layout'
+import RepositoryReleasesComparator from '~/components/RepositoryReleasesComparator'
+import { ComparatorProvider } from '~/contexts/comparator-context'
+
+const IndexPage = () => (
+  <Layout>
+    <NextSeo title="Comparator" />
+    <ComparatorProvider>
+      <RepositoryReleasesComparator />
+    </ComparatorProvider>
+  </Layout>
+)
+
+export default IndexPage
