6e627fed26d59ec96e0af9cd790c6bcba2d9c40e
Mario Beltrán Alarcón <belco90@gmail.com>
2021-07-16 19:42:50 +0200

    remove links from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,116 +1,14 @@
-import {
-  Box,
-  BoxProps,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerOverlay,
-  Flex,
-  Heading,
-  Icon,
-  IconButton,
-  Stack,
-  useBreakpointValue,
-  useDisclosure,
-} from '@chakra-ui/react'
+import { Box, BoxProps, Flex, Heading } from '@chakra-ui/react'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 import * as React from 'react'
-import { FaBars } from 'react-icons/fa'
 
 import mascotIcon from '@app-public/mascot-icon.png'
 import FluidContainer from '~/components/FluidContainer'
-import RouteLink from '~/components/RouteLink'
 import ToggleColorModeButton from '~/components/ToggleColorModeButton'
-import useIsClientSide from '~/hooks/useIsClientSide'
 
 const LOGO_SIZES = { base: '25px', md: '30px', lg: '50px' }
 
-const MenuLink = ({
-  href,
-  children,
-  ...rest
-}: {
-  children: React.ReactNode
-  href: string
-}) => {
-  const router = useRouter()
-  const isActive = router.pathname === href
-  const linkWidth = useBreakpointValue({ base: 'min-content', md: 'auto' })
-
-  return (
-    <RouteLink
-      href={href}
-      color="primaryTextLightmode"
-      borderBottomWidth={isActive ? '4px' : 'none'}
-      borderColor={isActive ? 'fuchsia.400' : 'fuchsia.200'}
-      fontWeight="black"
-      fontSize={{ base: '2xl', md: 'lg' }}
-      _hover={{
-        textDecoration: 'none',
-        borderBottomColor: 'fuchsia.500',
-        color: isActive ? 'none' : 'primaryTextLightmode',
-      }}
-      width={linkWidth}
-      aria-current={isActive ? 'page' : undefined}
-      {...rest}
-    >
-      {children}
-    </RouteLink>
-  )
-}
-
-const LinksStack = () => (
-  <Stack
-    direction={{ base: 'column', md: 'row' }}
-    spacing={{ base: 12, md: 8 }}
-    align={{ base: 'center', md: 'initial' }}
-  >
-    <MenuLink href="/">Comparator</MenuLink>
-    <MenuLink href="/about">About</MenuLink>
-    {!!process.env.NEXT_PUBLIC_FEATURE_FLAG_COLOR_MODE && (
-      <ToggleColorModeButton />
-    )}
-    {/* TODO: implement logout if necessary */}
-  </Stack>
-)
-
-const HeaderLinks = () => {
-  const { isOpen, onToggle, onClose } = useDisclosure()
-  const isDesktop = useBreakpointValue({ base: false, md: true })
-
-  const linksInner = <LinksStack />
-
-  return isDesktop ? (
-    linksInner
-  ) : (
-    <>
-      <IconButton
-        aria-label="Toggle menu"
-        icon={<Icon as={FaBars} />}
-        variant="unstyled"
-        colorScheme="coolGray.900"
-        size="lg"
-        onClick={onToggle}
-      />
-      <Drawer placement="right" isOpen={isOpen} size="full" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent py={8} backgroundColor="coolGray.100">
-          <DrawerCloseButton color="coolGray.900" size="lg" />
-          <DrawerBody>
-            <Flex justify="center" direction="column" h="full">
-              {linksInner}
-            </Flex>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </>
-  )
-}
-
 const Header = (props: BoxProps) => {
-  const isClientSide = useIsClientSide()
   return (
     <Box
       as="header"
@@ -139,7 +37,9 @@ const Header = (props: BoxProps) => {
               Octoclairvoyant
             </Heading>
           </Flex>
-          {isClientSide && <HeaderLinks />}
+          {!!process.env.NEXT_PUBLIC_FEATURE_FLAG_COLOR_MODE && (
+            <ToggleColorModeButton />
+          )}
         </Flex>
       </FluidContainer>
     </Box>
