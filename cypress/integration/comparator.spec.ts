ee28c9d4f97769f8dfe94c769d61ac711455d353
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-10-20 07:33:02 +0000

    Fix linting

--- cypress/integration/comparator.spec.ts
+++ cypress/integration/comparator.spec.ts
@@ -73,7 +73,6 @@ it('should show expected results when using standard query string', () => {
   cy.findByRole('heading', { level: 2, name: /chore/i })
 })
 
-
 // Keep the export {} braces here!
 
 export {}
