83de44a2acfaa74dcc854099522a524dbce53734
Mario Beltrán Alarcón <belco90@gmail.com>
2021-10-25 17:30:18 +0200

    fix CTA Button outline when focus on tab navigation

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -15,8 +15,10 @@ export const CTAButton = ({ children, ...remainingProps }: ButtonProps) => {
       bg="fuchsia.200"
       color="fuchsia.900"
       _hover={{ bg: 'fuchsia.100' }}
-      _active={{ bg: 'fuchsia.200' }}
-      _focus={{ boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.25) !important' }}
+      _active={{
+        bg: 'fuchsia.200',
+        boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.25) !important',
+      }}
     >
       {children}
     </Button>
