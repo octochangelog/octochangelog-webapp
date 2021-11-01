71edcbdaa74dc71255cabc776715a0d67575bb79
Mario Beltrán Alarcón <belco90@gmail.com>
2021-11-02 00:38:20 +0100

    forward ref in Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,5 +1,10 @@
 import { Link as ChakraLink, LinkProps } from '@chakra-ui/react'
+import { forwardRef } from 'react'
 
-const Link = (props: LinkProps) => <ChakraLink color="primary.500" {...props} />
+const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
+  <ChakraLink color="primary.500" {...props} ref={ref} />
+))
+
+Link.displayName = 'Link'
 
 export default Link
