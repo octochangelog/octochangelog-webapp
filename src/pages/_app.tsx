5020962431a81a32d04907c0940020629a4c2c45
Mario Beltrán Alarcón <belco90@gmail.com>
2021-11-09 19:47:15 +0000

    remove google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { resetIdCounter } from 'downshift'
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import 'focus-visible/dist/focus-visible'
 
 import { GithubAuthProvider } from '~/contexts/github-auth-provider'
 import customTheme from '~/customTheme'
-import * as gtag from '~/lib/gtag'
 import DefaultSEO from '~/next-seo.config'
 
 const queryClient = new QueryClient({
@@ -24,19 +21,6 @@ const queryClient = new QueryClient({
 })
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageView(url)
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   resetIdCounter()
 
   return (
