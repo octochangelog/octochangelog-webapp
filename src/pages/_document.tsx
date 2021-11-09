997ed512356cfba53e3653efd3fbcd07d9d4c181
Mario Beltrán Alarcón <belco90@gmail.com>
2021-11-09 21:35:27 +0100

    remove google analytics (#542)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -92,24 +92,6 @@ class MyDocument extends Document {
             content="/favicon/ms-icon-144x144.png"
           />
           <meta name="theme-color" content="#ac35b0" />
-
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body>
           <ColorModeScript
