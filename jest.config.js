a7e02991b711ee4ad7a2c782b2f69e1d9d16ad5f
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-12 11:16:24 +0100

    Setup jest (#659)
    * setup jest
    
    * avoid clashing types between cypress and jest
    
    * set root dir for jest
    
    * set up ESLint plugins for jest
    
    * minor improvements
    
    * rework TS config files
    
    * wrap jest config with next built-in util
    
    * add tests for first util function
    
    * add tests job to CI
    
    * rename tests job
    
    * regenerate lock

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  clearMocks: true,
+  rootDir: './src',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/$1',
+  },
+}
+
+module.exports = createJestConfig(config)
