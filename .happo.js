d776230c08520af66b331089785acbe79358e8c8
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-25 20:34:45 +0100

    add viewport to safari browser target

--- .happo.js
+++ .happo.js
@@ -1,18 +1,23 @@
 const { RemoteBrowserTarget } = require('happo.io')
 
+const DESKTOP_VIEWPORT = '1024x768'
+const MOBILE_VIEWPORT = '320x640'
+
 module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
   targets: {
     'Chrome - Desktop': new RemoteBrowserTarget('chrome', {
-      viewport: '1024x768',
+      viewport: DESKTOP_VIEWPORT,
     }),
     'Firefox - Desktop': new RemoteBrowserTarget('firefox', {
-      viewport: '1024x768',
+      viewport: DESKTOP_VIEWPORT,
     }),
     'Chrome - Mobile': new RemoteBrowserTarget('chrome', {
-      viewport: '320x640',
+      viewport: MOBILE_VIEWPORT,
     }),
-    'Safari - Mobile': new RemoteBrowserTarget('ios-safari'),
+    'Safari - Mobile': new RemoteBrowserTarget('ios-safari', {
+      viewport: MOBILE_VIEWPORT,
+    }),
   },
 }
