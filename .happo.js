a66adaee185684c10a148441f7e84de962a2d606
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-25 19:01:16 +0100

    add mobile targets for Happo

--- .happo.js
+++ .happo.js
@@ -4,11 +4,15 @@ module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
   targets: {
-    chrome: new RemoteBrowserTarget('chrome', {
+    'Chrome - Desktop': new RemoteBrowserTarget('chrome', {
       viewport: '1024x768',
     }),
-    firefox: new RemoteBrowserTarget('firefox', {
+    'Firefox - Desktop': new RemoteBrowserTarget('firefox', {
       viewport: '1024x768',
     }),
+    'Chrome - Mobile': new RemoteBrowserTarget('chrome', {
+      viewport: '320x640',
+    }),
+    'Safari - Mobile': new RemoteBrowserTarget('ios-safari'),
   },
 }
