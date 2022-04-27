a38a52972a92ca3eef4b28f1eb48a5f563195d1e
Mario Beltrán Alarcón <belco90@gmail.com>
2022-04-27 20:19:16 +0200

    try increasing the wait time

--- cypress/integration/comparator.spec.ts
+++ cypress/integration/comparator.spec.ts
@@ -26,7 +26,7 @@ it('should show expected results when using standard query string', () => {
     '/?repo=testing-library%2Fdom-testing-library&from=v6.16.0&to=v8.1.0'
   )
 
-  cy.wait(10000) // eslint-disable-line cypress/no-unnecessary-waiting
+  cy.wait(15000) // eslint-disable-line cypress/no-unnecessary-waiting
 
   // Confirm repository name is displayed
 
