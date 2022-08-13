339be765dbc58232682b566cb2e4f3a7aab8778b
Mario Beltrán Alarcón <belco90@gmail.com>
2022-08-13 10:56:16 +0200

    format with tabs

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,16 +4,16 @@ import { defineConfig } from 'cypress'
 import happoTask from 'happo-cypress/task'
 
 export default defineConfig({
-  projectId: 'u8grd8',
-  videoUploadOnPasses: false,
-  e2e: {
-    /**
-     * `on` is used to hook into various events Cypress emits
-     * `config` is the resolved Cypress config
-     */
-    setupNodeEvents(on) {
-      happoTask.register(on)
-    },
-    baseUrl: 'http://localhost:3000',
-  },
+	projectId: 'u8grd8',
+	videoUploadOnPasses: false,
+	e2e: {
+		/**
+		 * `on` is used to hook into various events Cypress emits
+		 * `config` is the resolved Cypress config
+		 */
+		setupNodeEvents(on) {
+			happoTask.register(on)
+		},
+		baseUrl: 'http://localhost:3000',
+	},
 })
