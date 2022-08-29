08d395f55d90f4cc8477410a7d8bbfcb524e83f7
Mario Beltrán Alarcón <belco90@gmail.com>
2022-08-29 15:11:19 +0200

    throw error on unhandled requests to github api

--- src/msw/index.ts
+++ src/msw/index.ts
@@ -1,12 +1,30 @@
+import type { MockedRequest } from 'msw'
+
+function unhandledRequestCallback(req: MockedRequest) {
+	if (req.url.host.includes('api.github.com')) {
+		throw new Error('There must be no unhandled request to GitHub API')
+	}
+
+	if (
+		req.url.host.includes('localhost') ||
+		req.url.host.includes('octoclairvoyant') ||
+		req.url.host.includes('fonts')
+	) {
+		return undefined
+	}
+
+	throw new Error('Unknown unhandled request')
+}
+
 async function initMocks() {
 	const isServerEnv = typeof window === 'undefined'
 
 	if (isServerEnv) {
 		const { server } = await import('./server')
-		server.listen()
+		server.listen({ onUnhandledRequest: unhandledRequestCallback })
 	} else {
 		const { worker } = await import('./browser')
-		void worker.start()
+		void worker.start({ onUnhandledRequest: unhandledRequestCallback })
 	}
 }
 
