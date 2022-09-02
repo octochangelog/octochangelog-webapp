6a709fd4155312d0bbd8f1775005fe1a268395cd
Mario Beltrán Alarcón <belco90@gmail.com>
2022-09-02 12:13:47 +0200

    setup ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,24 @@
 import type { Config } from 'jest'
 import nextJest from 'next/jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.json'
 
 const createJestConfig = nextJest({
 	dir: './',
 })
 
+const tsPathsToModules = pathsToModuleNameMapper(compilerOptions.paths, {
+	prefix: '<rootDir>/',
+})
+
 const config: Config = {
+	preset: 'ts-jest',
 	clearMocks: true,
 	roots: ['<rootDir>/src/'],
 	reporters: ['default', 'github-actions'],
 	moduleNameMapper: {
-		'~/(.*)': '<rootDir>/src/$1',
+		...tsPathsToModules,
 		'^lodash-es$': 'lodash', // so lodash-es is not compiled
 	},
 }
