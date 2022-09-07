75bc72b0123d1728ab9931ec7d40c4a9710b4850
Mario Beltrán Alarcón <belco90@gmail.com>
2022-09-07 21:13:34 +0200

    remove unnecessary cookie mock for cypress e2e

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -13,14 +13,4 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-import { GITHUB_STORAGE_KEY } from '~/github-client'
 import './commands'
-
-// Set GitHub token for all tests
-before(() => {
-	const githubTestingAccessToken = String(
-		Cypress.env('GITHUB_TESTING_ACCESS_TOKEN') || ''
-	)
-	cy.setCookie(GITHUB_STORAGE_KEY, githubTestingAccessToken)
-})
-Cypress.Cookies.defaults({ preserve: GITHUB_STORAGE_KEY })
