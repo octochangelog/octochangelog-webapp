ffc987f4f3c7d95d914945ef2c22b2dfa55751af
Mario Beltrán Alarcón <belco90@gmail.com>
2022-09-10 18:14:02 +0200

    merge sentry wizard into current config

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,16 @@ const config = {
 			},
 		]
 	},
+
+	sentry: {
+		// Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+		// for client-side builds. (This will be the default starting in
+		// `@sentry/nextjs` version 8.0.0.) See
+		// https://webpack.js.org/configuration/devtool/ and
+		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+		// for more information.
+		hideSourceMaps: true,
+	},
 }
 
 export default withSentryConfig(config, {
@@ -30,7 +40,7 @@ export default withSentryConfig(config, {
 	//   release, url, org, project, authToken, configFile, stripPrefix,
 	//   urlPrefix, include, ignore
 
-	silent: !process.env.VERCEL_ENV, // Suppresses all logs if not building the app at Vercel
+	silent: true, // Suppresses all logs
 	deploy: {
 		env: process.env.VERCEL_ENV,
 	},
