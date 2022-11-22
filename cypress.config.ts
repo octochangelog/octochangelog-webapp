fd9d317ce7dfc005b886c4fa60f4be0886a19482
Mario Beltrán Alarcón <belco90@gmail.com>
2022-11-22 10:04:57 +0000

    Update Cypress baseUrl
    Since Node v17 the resolution of localhost changed, so we need to use 127.0.0.1 in Cypress so Node serves the images properly.
    
    More info: https://stackoverflow.com/questions/72648487/node-js-16-17-changed-resolution-of-localhost

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
 			happoTask.register(on)
 		},
-		baseUrl: 'http://localhost:3000',
+		baseUrl: '127.0.0.1',
 		retries: {
 			runMode: 1,
 		},
