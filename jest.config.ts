806d946a2eab05584a78cc9c63f2f8b45d989581
Mario Beltrán <belco90@gmail.com>
2023-08-30 11:44:19 +0200

    Set GH Action reporter correctly
    Set silent to false and include the summary:
    https://jestjs.io/docs/configuration/#github-actions-reporter

--- jest.config.ts
+++ jest.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
 	preset: 'ts-jest',
 	clearMocks: true,
 	roots: ['<rootDir>/src/'],
-	reporters: ['default', 'github-actions'],
+	reporters: [['github-actions', {silent: false}], 'summary'],
 	setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
 	moduleNameMapper: {
 		...tsPathsToModules,
