179c9d6168acc96a1e5e4f8fc3edf3f4e8282a6c
Mario Beltrán Alarcón <belco90@gmail.com>
2023-10-26 17:54:04 +0200

    Load custom fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,28 @@
-import { type ReactNode } from 'react'
+import { Inter, Roboto_Mono } from 'next/font/google'
+import { type FC, type ReactNode } from 'react'
+
+const interFont = Inter({
+	subsets: ['latin'],
+	variable: '--font-inter',
+})
+
+const robotoMonoFont = Roboto_Mono({
+	subsets: ['latin'],
+	variable: '--font-roboto-mono',
+})
 
 export const metadata = {
 	title: 'Home',
 	description: 'Welcome to App router',
 }
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	)
-}
+const RootLayout: FC<{ children: ReactNode }> = ({ children }) => (
+	<html
+		lang="en"
+		className={`${interFont.variable} ${robotoMonoFont.variable}`}
+	>
+		<body>{children}</body>
+	</html>
+)
+
+export default RootLayout
