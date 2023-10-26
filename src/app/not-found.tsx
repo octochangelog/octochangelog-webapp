41c70574b8a86a0cd673d7019d08e9a1a284ba65
Mario Beltrán Alarcón <belco90@gmail.com>
2023-10-26 17:50:51 +0200

    Add initial not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { type FC } from 'react'
+
+const NotFound: FC = () => {
+	return (
+		<div>
+			<h2>Not Found</h2>
+			<p>Could not find requested resource</p>
+		</div>
+	)
+}
+
+export default NotFound
