738ce58d23ecb153d576317b45dd41a71c382429
Mario Beltrán Alarcón <belco90@gmail.com>
2023-10-27 16:32:49 +0200

    Set metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@ import { ColorModeScript } from '@chakra-ui/react'
 import { Inter, Roboto_Mono } from 'next/font/google'
 import { type FC, type ReactNode } from 'react'
 
+import Providers from './Providers'
 import VercelAnalytics from './VercelAnalytics'
+import { openGraph } from './shared-metadata'
 
-import Providers from '~/app/Providers'
+import { BRIEF_DESCRIPTION, FULL_DESCRIPTION, SITE_TITLE } from '~/common'
 import customTheme from '~/customTheme'
 
 const interFont = Inter({
@@ -18,8 +20,16 @@ const robotoMonoFont = Roboto_Mono({
 })
 
 export const metadata = {
-	title: 'Home',
-	description: 'Welcome to App router',
+	title: {
+		template: `%s | ${SITE_TITLE}`,
+		default: `${SITE_TITLE}: ${BRIEF_DESCRIPTION}`,
+	},
+	description: FULL_DESCRIPTION,
+	openGraph: { ...openGraph },
+
+	// TODO: Move this to `generateViewport` in Next.js v14
+	//  https://nextjs.org/docs/app/api-reference/functions/generate-viewport
+	themeColor: customTheme.colors.primary['700'],
 }
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => (
---
+++ src/app/shared-metadata.ts
@@ -0,0 +1,18 @@
+import { type Metadata } from 'next'
+
+import { BRIEF_DESCRIPTION, SITE_TITLE } from '~/common'
+
+export const openGraph: NonNullable<Metadata['openGraph']> = {
+	type: 'website',
+	url: '/',
+	title: `${SITE_TITLE}: ${BRIEF_DESCRIPTION}`,
+	images: [
+		{
+			url: '/mascot-logo.png',
+			height: 600,
+			width: 600,
+			type: 'image/png',
+			alt: 'Octoclairvoyant mascot reading a crystal ball',
+		},
+	],
+}
