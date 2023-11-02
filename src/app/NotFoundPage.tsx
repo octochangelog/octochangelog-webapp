1fe9852ee0a09cbc58688ecba9309bbecee9baa6
Mario Beltrán <belco90@gmail.com>
2023-11-02 11:14:21 +0100

    Move the not found page

---
+++ src/app/NotFoundPage.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { Link } from '@chakra-ui/next-js'
+import {
+	Button,
+	Container,
+	Heading,
+	Stack,
+	Text,
+	VStack,
+} from '@chakra-ui/react'
+import Image from 'next/image'
+import NextLink from 'next/link'
+import { type FC } from 'react'
+
+import mascotIcon from '@app-public/mascot-icon.png'
+
+const NotFoundPage: FC = () => {
+	return (
+		<Container variant="fluid" pb={{ base: 8, lg: 16 }}>
+			<VStack px="10">
+				<Image src={mascotIcon} alt="" width={250} height={250} />
+				<Stack
+					shouldWrapChildren
+					alignItems="center"
+					spacing="2"
+					flexGrow={1}
+					flexBasis={0}
+				>
+					<Heading
+						as="h2"
+						color="primaryText"
+						fontSize="4xl"
+						fontWeight="black"
+						letterSpacing="tight"
+					>
+						This page could not be found.
+					</Heading>
+					<Text as="p" color="secondaryText" fontSize="2xl" fontWeight="black">
+						Octoclairvoyant cannot divine the page you wanted.
+					</Text>
+				</Stack>
+			</VStack>
+			<VStack mt={12} spacing={5}>
+				<NextLink href="/comparator" passHref legacyBehavior>
+					<Button as="a" variant="cta">
+						Go to comparator
+					</Button>
+				</NextLink>
+
+				<Link href="/" fontWeight="black">
+					Or go to homepage
+				</Link>
+			</VStack>
+		</Container>
+	)
+}
+
+export default NotFoundPage
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,16 @@
 import { type FC } from 'react'
 
+import NotFoundPage from './NotFoundPage'
+import { openGraph } from './shared-metadata'
+
+export const metadata = {
+	title: 'Not Found',
+	description: 'This page could not be found',
+	openGraph: { ...openGraph, title: 'Not Found' },
+}
+
 const NotFound: FC = () => {
-	return (
-		<div>
-			<h2>Not Found</h2>
-			<p>Could not find requested resource</p>
-		</div>
-	)
+	return <NotFoundPage />
 }
 
 export default NotFound
