8a668754e74133a4b88fdc216156e68e5cf80610
Mario Beltrán <belco90@gmail.com>
2023-11-02 11:45:54 +0100

    Merge not-found in the original file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,19 @@
+import {
+	Button,
+	Container,
+	Heading,
+	Stack,
+	Text,
+	VStack,
+} from '@chakra-ui/react'
+import Image from 'next/image'
+import NextLink from 'next/link'
 import { type FC } from 'react'
 
-import NotFoundPage from './NotFoundPage'
 import { openGraph } from './shared-metadata'
+
+import mascotIcon from '@app-public/mascot-icon.png'
+import { Link } from '~/components/ChakraNextLink'
 
 export const metadata = {
 	title: 'Not Found',
@@ -10,7 +22,44 @@ export const metadata = {
 }
 
 const NotFound: FC = () => {
-	return <NotFoundPage />
+	return (
+		<Container variant="fluid" pb={{ base: 8, lg: 16 }}>
+			<VStack px="10">
+				<Image src={mascotIcon} alt="" width={250} height={250} />
+				<Stack
+					shouldWrapChildren
+					alignItems="center"
+					spacing="2"
+					flexGrow={1}
+					flexBasis={0}
+				>
+					<Heading
+						as="h2"
+						color="primaryText"
+						fontSize="4xl"
+						fontWeight="black"
+						letterSpacing="tight"
+					>
+						This page could not be found.
+					</Heading>
+					<Text as="p" color="secondaryText" fontSize="2xl" fontWeight="black">
+						Octoclairvoyant cannot divine the page you wanted.
+					</Text>
+				</Stack>
+			</VStack>
+			<VStack mt={12} spacing={5}>
+				<NextLink href="/comparator" passHref legacyBehavior>
+					<Button as="a" variant="cta">
+						Go to comparator
+					</Button>
+				</NextLink>
+
+				<Link href="/" fontWeight="black">
+					Or go to homepage
+				</Link>
+			</VStack>
+		</Container>
+	)
 }
 
 export default NotFound
