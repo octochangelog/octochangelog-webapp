07bd28eaef36ef0895a358f5de53ae41a5b0bc53
Mario Beltrán <belco90@gmail.com>
2023-11-08 17:03:58 +0100

    Add root error page

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,35 +1,16 @@
 'use client'
 
-import {
-	Button,
-	ColorModeScript,
-	Container,
-	Heading,
-	Stack,
-	Text,
-	VStack,
-} from '@chakra-ui/react'
-import * as Sentry from '@sentry/nextjs'
-import Image from 'next/image'
-import { type FC, useEffect } from 'react'
+import { ColorModeScript } from '@chakra-ui/react'
+import { type FC } from 'react'
 
-import Providers from '~/app/Providers'
-import UILayout from '~/app/UILayout'
 import customTheme from '~/customTheme'
 import { interFont, robotoMonoFont } from '~/fonts'
+import { type NextErrorPageProps } from '~/models'
 
-import mascotIcon from '@app-public/mascot-icon.png'
+import Providers from './Providers'
+import UIError from './UIError'
 
-type GlobalErrorProps = {
-	error: Error & { digest?: string }
-	reset: () => void
-}
-
-const GlobalError: FC<GlobalErrorProps> = ({ error, reset }) => {
-	useEffect(() => {
-		Sentry.captureException(error)
-	}, [error])
-
+const GlobalError: FC<NextErrorPageProps> = ({ error, reset }) => {
 	return (
 		<html
 			lang="en"
@@ -41,42 +22,7 @@ const GlobalError: FC<GlobalErrorProps> = ({ error, reset }) => {
 					initialColorMode={customTheme.config.initialColorMode}
 				/>
 				<Providers>
-					<UILayout>
-						<Container variant="fluid" pb={{ base: 8, lg: 16 }}>
-							<VStack px="10">
-								<Image src={mascotIcon} alt="" width={250} height={250} />
-								<Stack
-									shouldWrapChildren
-									alignItems="center"
-									spacing="2"
-									flexGrow={1}
-									flexBasis={0}
-								>
-									<Heading
-										as="h2"
-										color="primaryText"
-										fontSize="4xl"
-										fontWeight="black"
-										letterSpacing="tight"
-									>
-										Something went wrong!
-									</Heading>
-									<Text
-										as="p"
-										color="secondaryText"
-										fontSize="2xl"
-										fontWeight="black"
-									>
-										Octoclairvoyant could not handle the workload properly.
-									</Text>
-								</Stack>
-							</VStack>
-
-							<VStack mt={12} spacing={5}>
-								<Button onClick={() => reset()}>Try again</Button>
-							</VStack>
-						</Container>
-					</UILayout>
+					<UIError error={error} reset={reset} />
 				</Providers>
 			</body>
 		</html>
