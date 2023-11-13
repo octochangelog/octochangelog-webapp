eafd53a229ea9f0de6b93959e9b3bfbc0ac5989b
Mario Beltrán <belco90@gmail.com>
2023-11-13 14:38:29 +0100

    Remove useMsw hook

--- src/app/comparator/ComparatorClientView.tsx
+++ src/app/comparator/ComparatorClientView.tsx
@@ -1,26 +1,17 @@
 'use client'
 
-import { Box, CircularProgress, Flex } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 import RepositoryReleasesComparator from '~/app/comparator/RepositoryReleasesComparator'
-import { useMsw } from '~/hooks/useMsw'
 
 import { ComparatorProvider } from './comparator-context'
 
 const ComparatorClientView = () => {
-	const { isReady } = useMsw()
-
 	return (
 		<Box height="full" width="full" bgColor="background3">
-			{isReady ? (
-				<ComparatorProvider>
-					<RepositoryReleasesComparator />
-				</ComparatorProvider>
-			) : (
-				<Flex align="center" justify="center" height="100%">
-					<CircularProgress isIndeterminate size="8" color="primary.400" />
-				</Flex>
-			)}
+			<ComparatorProvider>
+				<RepositoryReleasesComparator />
+			</ComparatorProvider>
 		</Box>
 	)
 }
