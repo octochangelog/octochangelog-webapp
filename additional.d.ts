38cb60ff4cd3be1ccf13dadab9b5836098ba12b6
Mario Beltrán Alarcón <belco90@gmail.com>
2023-11-15 08:34:04 +0100

    Remove msw attributes from window

--- additional.d.ts
+++ additional.d.ts
@@ -2,8 +2,6 @@
  * @file Declare here any additional module with no types or global types.
  */
 
-import { type SetupWorker, type rest } from 'msw'
-
 declare module 'happo-cypress/task' {
 	interface HappoTask {
 		register: (on: Cypress.PluginEvents) => void
@@ -11,16 +9,4 @@ declare module 'happo-cypress/task' {
 
 	const happoTask: HappoTask
 	export default happoTask
-}
-
-declare global {
-	interface Window {
-		// msw utils passed through window to be reused in E2E or other parts.
-		msw?: {
-			worker: SetupWorker
-			rest: typeof rest
-		}
-		isApiMockingEnabled?: boolean
-		isApiMockingReady?: boolean
-	}
 }
