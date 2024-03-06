3258b27e6048cd2311b8304c039ccbf9e289d512
Mario Beltrán <belco90@gmail.com>
2024-03-06 13:18:44 +0100

    Simplify metadata

--- src/app/shared-metadata.ts
+++ src/app/shared-metadata.ts
@@ -5,7 +5,8 @@ import { BRIEF_DESCRIPTION, SITE_TITLE } from '@/common'
 export const openGraph: NonNullable<Metadata['openGraph']> = {
 	type: 'website',
 	url: '/',
-	title: `${SITE_TITLE}: ${BRIEF_DESCRIPTION}`,
+	title: SITE_TITLE,
+	description: BRIEF_DESCRIPTION,
 	images: [
 		{
 			url: '/mascot-icon.png',
