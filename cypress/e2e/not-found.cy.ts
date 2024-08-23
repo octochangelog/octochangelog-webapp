d7ea982b1b1515ddfddf346d477f1a3e4d66429c
Mario Beltrán <belco90@gmail.com>
2024-08-23 11:04:53 +0200

    Use correct image role

--- cypress/e2e/not-found.cy.ts
+++ cypress/e2e/not-found.cy.ts
@@ -1,33 +1,33 @@
 it('should display custom "not found" page for non-existing page', () => {
-	cy.visit('/not-existing-page', { failOnStatusCode: false })
+	cy.visit('/not-existing-page', { failOnStatusCode: false });
 	cy.metaDescriptionShouldEqual(
-		'Compare GitHub changelogs across multiple releases in a single view',
-	)
-	cy.title().should('equal', 'Not Found | Octochangelog')
+		'Compare GitHub changelogs across multiple releases in a single view'
+	);
+	cy.title().should('equal', 'Not Found | Octochangelog');
 
 	cy.findByRole('heading', {
 		name: 'This page could not be found.',
-	}).should('exist')
+	}).should('exist');
 
 	cy.findByText('Octochangelog cannot divine the page you wanted.').should(
-		'exist',
-	)
+		'exist'
+	);
 
 	cy.findByRole('main').within(() => {
-		cy.findByRole('img')
+		cy.findByRole('presentation')
 			.should('have.attr', 'src')
-			.and('contain', 'mascot-icon')
-	})
+			.and('contain', 'mascot-icon');
+	});
 
 	cy.findByRole('link', { name: /go to compare/i }).should(
 		'have.attr',
 		'href',
-		'/compare',
-	)
+		'/compare'
+	);
 
 	cy.findByRole('link', { name: 'Or go to homepage' }).should(
 		'have.attr',
 		'href',
-		'/',
-	)
-})
+		'/'
+	);
+});
